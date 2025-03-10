db6bddf6e7c1a84e879e53a699cb6bc91288f474
Daniel Davis Andersen <danie@881F7E464898EE7.UEhlwji6Ki>
2025-03-10 17:34:19 +0100

    Fixed Typo

--- Settings.cs
+++ Settings.cs
@@ -102,7 +102,7 @@ namespace WaystoneModTracker
             MapMonstersPctAllDamageToGainAsCold = new ToggleNode(false);
             MapMonstersPctAllDamageToGainAsFire= new ToggleNode(false);
             MapMonstersAdditionalElementalResistance= new ToggleNode(false);
-            MapExperinceGainPct= new ToggleNode(false);
+            MapExperienceGainPct= new ToggleNode(false);
         }
 
         [Menu("Enable Plugin")]
@@ -123,7 +123,7 @@ namespace WaystoneModTracker
         [Menu("UI Position Y")]
         public RangeNode<int> Y { get; set; }
         // 🔹 Map Mod Checkboxes 
-        [Menu("Map Experince Gain %")] public ToggleNode MapExperinceGainPct { get; set; }
+        [Menu("Map Experince Gain %")] public ToggleNode MapExperienceGainPct { get; set; }
         [Menu("Map Monsters Elemental Resistance")] public ToggleNode MapMonstersAdditionalElementalResistance { get; set; }
         [Menu("Map Monsters Damage As Extra Fire %")] public ToggleNode MapMonstersPctAllDamageToGainAsFire { get; set; }
         [Menu("Map Monsters Damage As Extra Cold %")] public ToggleNode MapMonstersPctAllDamageToGainAsCold { get; set; }
